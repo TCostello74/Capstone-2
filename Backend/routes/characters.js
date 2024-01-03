e955a3009aa2b5e86929fc8006b14b4d8177a2f4
tcostello74 <tcostello74@yahoo.com>
2024-01-03 11:37:14 -0500

    Edits

--- Backend/routes/characters.js
+++ Backend/routes/characters.js
@@ -5,13 +5,14 @@ const axios = require('axios');
 
 
 
-app.get('/characters', async (req, res) => {
+router.get('/characters', async (req, res) => {
     try {
       const response = await axios.get('https://the-one-api.dev/v2/character', {
         headers: {
           'Authorization': `Bearer ${process.env.API_KEY}`
         }
       });
+      console.log(response.data);
       res.json(response.data);
     } catch (error) {
       res.status(500).json({ message: error.message });
