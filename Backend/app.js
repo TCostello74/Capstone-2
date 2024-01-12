a04edaf464cb311cf95cae3e61e9b1a8cacc3ce8
tcostello74 <tcostello74@yahoo.com>
2024-01-12 11:18:47 -0500

    Edits-4

--- Backend/app.js
+++ Backend/app.js
@@ -1,40 +1,50 @@
 const express = require('express');
+const ExpressError = require("./expressError");
 require('dotenv').config();
-
+const { authenticateJWT } = require("./middleware/auth")
 
 //Initializes new Express application
 const app = express();
-const ExpressError = require("./expressError")
+//Middleware to parse incoming JSON payloads
+app.use(express.json());
+app.use(authenticateJWT);
+
+const userRoutes = require('./routes/users');
 const characterRoutes = require('./routes/characters');
 const quotesRoutes = require('./routes/quotes');
 const moviesRoutes = require('./routes/movies');
 const booksRoutes = require('./routes/books');
+const triviaRoutes = require('./routes/triviaQuestions')
 
-
-//Middleware to parse incoming JSON payloads
-app.use(express.json());
 //Set up Routes
+
+app.use('/users', userRoutes);
 app.use('/characters', characterRoutes);
 app.use('/quotes', quotesRoutes);
 app.use('/movies', moviesRoutes);
 app.use('/books', booksRoutes);
+app.use('/triviaQuestions', triviaRoutes);
 
 
-app.use(function(req, res, next) { //defines middleware function
-    const err = new ExpressError("Not Found", 404); //catches any request not handled by other routes
-    return next(err); //passed to next function in middleware stack
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not found!",404);
+   return next(err);
+});
+
+/** general error handler */
+
+app.use(function(err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status
+    }
   });
-  
-  /** general error handler */
-  
-  app.use((err, req, res, next) => { //general error handling middleware
-    res.status(err.status || 500); //called when preceding middleware function calls 'next'
-  
-    return res.json({
-      error: err,
-      message: err.message
-    });
-  });
-  
-  
-  module.exports = app;
+});
+
+
+module.exports = app;
