e4b0a8dd65cbed64de6a0d27876ba4f36b3cca19
tcostello74 <tcostello74@yahoo.com>
2024-01-12 11:16:39 -0500

    User-tests

--- Backend/routes/users.test.js
+++ Backend/routes/users.test.js
@@ -8,7 +8,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
-// use small value here so tests are fast
+// small value to ensure tests are fast
 const BCRYPT_WORK_FACTOR = 1;
 
 let testUserToken;
@@ -22,20 +22,19 @@ beforeEach(async function () {
   await db.query(`INSERT INTO users VALUES ('admin', $1)`,
     [hashedPassword]);
 
-  // we'll need tokens for future requests
+  // test tokens
   const testUser = { username: "test" };
   const testAdmin = { username: "admin" };
   testUserToken = jwt.sign(testUser, SECRET_KEY);
   testAdminToken = jwt.sign(testAdmin, SECRET_KEY);
 });
-// end
 
-// POST /register returns `{username}` 
+/** POST /register returns `{username}` */
 
-describe('POST /register', function () {
+describe('POST /users/register', function () {
   test("returns {username}", async function () {
     const response = await request(app)
-      .post(`/register`)
+      .post(`/users/register`)
       .send({ username: "test2", password: "secret2" });
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ username: "test2" });
@@ -43,25 +42,47 @@ describe('POST /register', function () {
 });
 
 
-// POST /login returns `{message: "Logged in!"} 
+/** POST /login returns a JWT token **/
 
-describe('POST /login', function () {
+describe('POST /users/login', function () {
   test("returns logged in msg", async function () {
     const response = await request(app)
-      .post(`/login`)
+      .post(`/users/login`)
       .send({ username: "test", password: "secret" });
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ token: expect.any(String) });
+    expect(response.body).toEqual(expect.objectContaining({ token: expect.any(String) }));
   });
 
   test("fails with wrong password", async function () {
     const response = await request(app)
-      .post(`/login`)
+      .post(`/users/login`)
       .send({ username: "test", password: "WRONG" });
     expect(response.statusCode).toBe(400);
   });
 });
 
+
+describe("GET /users/topsecret", function () {
+  test("returns 'Made it'", async function () {
+    const response = await request(app)
+      .get(`/users/topsecret`)
+      .send({ _token: testUserToken });
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("returns 401 when logged out", async function () {
+    const response = await request(app)
+      .get(`/users/topsecret`); // no token being sent!
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("returns 401 with bad token", async function () {
+    const response = await request(app)
+      .get(`/users/topsecret`)
+      .send({ _token: "garbage" }); // invalid token!
+    expect(response.statusCode).toBe(401);
+  });
+});
 
 afterEach(async function () {
   // delete any data created by test
