4666f9298f1e1c89ef9fd180081178848a287962
tcostello74 <tcostello74@yahoo.com>
2024-01-12 11:11:33 -0500

    error handling

--- Backend/expressError.js
+++ Backend/expressError.js
@@ -1,17 +1,12 @@
-/** ExpressError extends the normal JS error to easily
- *  add a status when we make an instance of it.
- *
- *  The error-handling middleware will return this.
- */
-
 class ExpressError extends Error {
-    constructor(message, status) {
-      super();
-      this.message = message;
-      this.status = status;
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    if (process.env.NODE_ENV != "test") {
       console.error(this.stack);
     }
   }
-  
-  
-  module.exports = ExpressError;
+}
+
+module.exports = ExpressError;
