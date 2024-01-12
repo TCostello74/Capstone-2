85d1279f992b1716096124fc87962e914d22f3d1
tcostello74 <tcostello74@yahoo.com>
2024-01-12 11:17:58 -0500

    Auth-JWT

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -2,47 +2,24 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const ExpressError = require("../expressError");
 
-// Auth JWT token, add auth'd user (if any) to req
-
 function authenticateJWT(req, res, next) {
-  try {
-    const tokenFromBody = req.body._token;
-    const payload = jwt.verify(tokenFromBody, SECRET_KEY);
-    req.user = payload;
-    return next();
-  } catch (err) {
-    // error in this middleware isn't error -- continue on
-    return next();
-  }
+    try {
+      const payload = jwt.verify(req.body._token, SECRET_KEY);
+      req.user = payload;
+      console.log("YOU HAVE A VALID TOKEN!!")
+      return next();
+    } catch (e) {
+      return next ();
+    }
 }
-
-
-//Require user or raise 401 
 
 function ensureLoggedIn(req, res, next) {
-  if (!req.user) {
-    const err = new ExpressError("Unauthorized", 401);
-    return next(err);
-  } else {
-    return next();
-  }
+    if(!req.user){
+      const e = new ExpressError("Unauthorized", 401);
+      return next(e);
+    } else {
+      return next();
+    }
 }
 
-
-// Require admin user or raise 401 
-
-function ensureAdmin(req, res, next) {
-  if (!req.user || req.user.username != "admin") {
-    const err = new ExpressError("Unauthorized", 401);
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-
-module.exports = {
-  authenticateJWT,
-  ensureLoggedIn,
-  ensureAdmin,
-};
+module.exports = { authenticateJWT, ensureLoggedIn };
