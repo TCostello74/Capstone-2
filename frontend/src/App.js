85f1cdfc11210df2e810ce0b829cd64360bfeea9
tcostello74 <tcostello74@yahoo.com>
2024-01-20 17:57:10 -0500

    Edits-3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthContext';
 import NavBar from './routes-nav/NavBar';
 import Routes from './routes-nav/Routes';
 import './App.css';
@@ -8,12 +9,14 @@ function App() {
   const [score, setScore] = useState(0);
 
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Routes setScore={setScore} score={score} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes setScore={setScore} score={score} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
